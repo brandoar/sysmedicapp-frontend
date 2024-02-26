6d4fcac28c67b9a20039fab7f299beafb746a5d0
Brandon Abregu Ramos <brandoabreguramos@gmail.com>
2024-02-25 23:01:33 -0500

    VERTICAL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    URL_BACKEND: 'https://api.cursos-udemy.site',
-    URL_SERVICIOS: 'https://api.cursos-udemy.site/api',
-    URL_FRONTEND: 'https://admin.cursos-udemy.site/'
+    URL_BACKEND: 'http://18.191.139.37:8000/',
+    URL_SERVICIOS: 'http://18.191.139.37:8000/api',
+    URL_FRONTEND: 'http://18.191.139.37/'
 };
